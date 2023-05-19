f846fee17335d9680607fe71cadbcfb24f7b8241
Alber321 <lsolis2@ucol.mx>
2023-05-19 17:06:08 -0600

    Avances react 2

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, {Fragment,useState, useEffect} from 'react';
 import Navbar from './Components/Navbar';
+import UserList from './Components/UserList';
 
 function App() {
-  return (
 
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUsers  = () => {
+      fetch('http://localhost:3000/api/users')
+      .then(res => res.json())
+      .then(res => setUsers(res))
+    }
+    getUsers()
+  }, [])
+  
+
+  return (
+    <Fragment>
+      <Navbar brand='CRUD-Luis'/>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-7'>
+            <h2 style={{textAlign: 'center'}}>User List</h2>
+            <UserList/>
+          </div>
+          <div className='col-5'>
+          <h2 style={{textAlign: 'center'}}>Formulario de Usuario</h2>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ crud-app/src/Components/UserList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const UserList = () => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>LastName</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>LastName</th>
+          <th>Email</th>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
